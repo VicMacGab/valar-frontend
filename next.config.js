c35eb6511d9283fb0383694a0ff8e4097b8f0092
fresh-avocado <gabriel.spranger@utec.edu.pe>
2021-11-19 16:38:11 -0500

    backend url conditional to NODE_ENV

--- next.config.js
+++ next.config.js
@@ -4,6 +4,9 @@ module.exports = {
   poweredByHeader: false,
   // acá se definen env variables que estarán disponibles solo para el front
   env: {
-    BACKEND_URL: "http://localhost:5000/api",
+    BACKEND_URL:
+      process.env.NODE_ENV == "production"
+        ? "https://ec2-18-216-243-84.us-east-2.compute.amazonaws.com/api"
+        : "http://localhost:5000/api",
   },
 };
